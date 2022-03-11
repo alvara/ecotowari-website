a979b9b324fd5338f0ce4c664cea00c582f83a32
Jesse Alvarado <contact@jessealvarado.com>
2022-03-11 23:16:58 +0900

    setup data display

--- web/modules/sections/Statistics.tsx
+++ web/modules/sections/Statistics.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { avgWastePerDay } from '../../utils/calculations'
+import {waterSaved, electricitySaved, co2Saved, flyersReduced} from '../../utils/calculations'
 
 Statistics.propTypes = {
   stickers: PropTypes.arrayOf(PropTypes.object),
@@ -15,8 +15,10 @@ export default function Statistics({stickers}) {
       <span className='preTitle'>Ecotowari in Numbers</span>
       <h2>Check how much resources have saved with ecotowari sticker!</h2>
       <div className="row ow-cols-sm-4 row-cols-md-2 row-cols-lg-3">
-      {avgWastePerDay(stickers)}
-
+      Water saved: {Math.round(waterSaved(stickers))} L <br />
+      Electricity saved: {Math.round(electricitySaved(stickers))} kWh <br />
+      CO2 Saved: {Math.round(co2Saved(stickers))} kg-CO2e <br />
+      Flyers reduced: {Math.round(flyersReduced(stickers))} kg <br />
       </div>
   </div>
   )
