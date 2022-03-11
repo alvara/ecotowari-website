12d036f5359f5286853c15de69392ab921672a88
Jesse Alvarado <contact@jessealvarado.com>
2022-03-11 22:08:22 +0900

    implement days deployed

--- web/utils/calculations.tsx
+++ web/utils/calculations.tsx
@@ -1,6 +1,9 @@
+import dayjs from 'dayjs'
+
 // return number of days a sticker has been deployed on the field
 export function daysDeployed(startedAt: Date):number {
-  // return Date.now() - startedAt.toUTCString
+  const now = dayjs()
+  return now.diff(startedAt, 'day')
 }
 
 // returns avg weight of flyers wasted
