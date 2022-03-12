efe586082756d1899885adfad4d293881fe567d3
Jesse Alvarado <contact@jessealvarado.com>
2022-03-12 10:47:01 +0900

    setup home page header section schema

--- studio/schemas/documents/home.js
+++ studio/schemas/documents/home.js
@@ -1,22 +1,38 @@
 import bcp47 from 'bcp47';
 
-
-
 export default {
   name: 'home-page',
   type: 'document',
   title: 'Home Page',
   fields: [
     {
-      title: 'Title',
-      name: 'title',
+      title: 'Header Section',
+      name: 'headersection',
+      description: 'This is the top header section that everyone sees when they first visit the website.',
       type: 'object',
       fields: [
         {
           title: 'Title',
           name: 'title',
           type: 'localeString'
-        }
+        },
+        {
+          title: 'Subtitle',
+          name: 'subtitle',
+          type: 'localeString'
+        },
+        {
+          title: 'Button Text',
+          description: 'What does the button say?',
+          name: 'buttontext',
+          type: 'localeString'
+        },
+        {
+          title: 'Button URL Path',
+          name: 'buttonpath',
+          type: 'string'
+        },
+
       ]
     }
   ],
