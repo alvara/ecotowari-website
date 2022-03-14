bfdf0e19fddd649f3c1f9decc493b513bb22266d
Jesse Alvarado <contact@jessealvarado.com>
2022-03-14 17:13:54 +0900

    add headers to environment sections

--- web/modules/sections/EnvironmentImpact.tsx
+++ web/modules/sections/EnvironmentImpact.tsx
@@ -19,19 +19,32 @@ export default function EnvironmentImpact({data}) {
       {data.content[router.locale]}
       </p>
         <div className="col-md-6 p-5 bg-primary">
-          <FontAwesomeIcon icon={faDroplet} size="lg" className='environment-icon text-white'/>
+          <div className="d-flex align-items-center text-white">
+            <FontAwesomeIcon icon={faDroplet} size="lg" className='environment-icon'/>
+            <h3 className="text-white">Water</h3>
+          </div>
           <pre className='text-white'>{data.water[router.locale]}</pre>
         </div>
         <div className="col-md-6 p-5 border">
-          <FontAwesomeIcon icon={faLightbulb} className='environment-icon text-primary'/>
+          <div className='d-flex align-items-center textiwhite'>
+            <FontAwesomeIcon icon={faLightbulb} className='environment-icon text-primary'/>
+            <h3 className="text-primary">Electricity</h3>
+          </div>
           <pre className='text-primary'>{data.electricity[router.locale]}</pre>
         </div>
         <div className="col-md-6 p-5 border">
+          <div className='d-flex align-items-center textiwhite'>
             <FontAwesomeIcon icon={faFlask} className='environment-icon text-primary'/>
-            <pre className='text-primary'>{data.chemicals[router.locale]}</pre>
+            <h3 className="text-primary">Water</h3>
+          </div>
+          <pre className='text-primary'>{data.chemicals[router.locale]}</pre>
         </div>
         <div className="col-md-6 p-5 bg-primary">
+          <div className='d-flex align-items-center textiwhite'>
             <FontAwesomeIcon icon={faTree} className='environment-icon text-white'/>
+            <h3 className="text-white">Water</h3>
+
+          </div>
           <pre className='text-white'>{data.wood[router.locale]}</pre>
         </div>
       </div>
