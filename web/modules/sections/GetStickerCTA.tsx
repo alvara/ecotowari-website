71d72097a039f80ff6fbbda6557dffcefd296369
Jesse Alvarado <contact@jessealvarado.com>
2022-03-15 17:50:44 +0900

    style card shadow

--- web/modules/sections/GetStickerCTA.tsx
+++ web/modules/sections/GetStickerCTA.tsx
@@ -19,7 +19,7 @@ export default function GetStickerCTA({data}) {
       <span className="text-center preTitle">Get a sticker</span>
       <h2 className="text-center">{data.title[router.locale]}</h2>
         <div className="offset-md-2 col-md-8">
-          <Image src='/stickerv1.png' width="400" height="100" layout="responsive" objectFit='contain' alt="test" className="" quality={30} priority/>
+          <Image src='/stickerv1.png' width="400" height="100" layout="responsive" objectFit='contain' alt="test" className="card-shadow" quality={30} priority/>
           <p>
             <pre>{data.content[router.locale]}</pre>
           </p>
