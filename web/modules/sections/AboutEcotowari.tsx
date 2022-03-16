449d0a1c9fc119be02a70340eef3f191ad2f8355
Jesse Alvarado <contact@jessealvarado.com>
2022-03-17 00:01:37 +0900

    fix mobile view collapse

--- web/modules/sections/AboutEcotowari.tsx
+++ web/modules/sections/AboutEcotowari.tsx
@@ -14,10 +14,10 @@ export default function AboutEcotowari({data}) {
     <div className="row">
       <span className="text-center preTitle">Who We Are</span>
       <h2 className="text-center">{data.title[router.locale]}</h2>
-        <div className="col-6">
+        <div className="col-md-6">
           <pre>{data.content[router.locale]}</pre>
         </div>
-        <div className="col-6">
+        <div className="col-md-6">
           <Image src='/danger-flyers.jpeg' width="300" height="200" layout="responsive" objectFit='contain' alt="test" quality={30} priority/>
         </div>
       </div>
