0cdbd997a6768df09076a8430f6d9776881986cd
Jesse Alvarado <contact@jessealvarado.com>
2022-04-06 17:43:11 +0900

    fix missing parallax bg for sticker section

--- web/modules/sections/GetStickerCTA.tsx
+++ web/modules/sections/GetStickerCTA.tsx
@@ -1,30 +1,40 @@
 import React from 'react'
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types'
 import {useRouter} from 'next/router'
 import Image from 'next/image'
-import MailchimpSubscribe from "react-mailchimp-subscribe"
+import MailchimpSubscribe from 'react-mailchimp-subscribe'
 
 GetStickerCTA.propTypes = {
-  data: PropTypes.object
+  data: PropTypes.object,
 }
 
 export default function GetStickerCTA({data}) {
   const router = useRouter()
 
-  const url = "https://ecotowari.us14.list-manage.com/subscribe/post?u=fb888509b6f45d3e61ff06e47&amp;id=5ec65eaa39";
-
+  const url =
+    'https://ecotowari.us14.list-manage.com/subscribe/post?u=fb888509b6f45d3e61ff06e47&amp;id=5ec65eaa39'
 
   return (
     <div id="sticker" className="row">
       <span className="text-center preTitle">Get a sticker</span>
       <h2 className="text-center">{data.title[router.locale]}</h2>
-        <div className="offset-md-2 col-md-8">
-          <Image src='/stickerv1.png' width="400" height="100" layout="responsive" objectFit='contain' alt="test" quality={30} priority/>
-          <pre>{data.content[router.locale]}</pre>
-          <div className="text-center mailingList">
-            <MailchimpSubscribe url={url}/>
-          </div>
+      <div className="offset-md-2 col-md-8">
+        <Image
+          src="/stickerv1.png"
+          width="400"
+          height="100"
+          layout="responsive"
+          objectFit="contain"
+          alt="test"
+          quality={30}
+          priority
+        />
+        <pre>{data.content[router.locale]}</pre>
+        <div className="text-center mailingList">
+          <MailchimpSubscribe url={url} />
         </div>
       </div>
+      <div className="bg-overlay"></div>
+    </div>
   )
 }
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -113,7 +113,7 @@ export default function Index({posts, stickers, igPosts, homePage}) {
       <Container wrapperClass="min-h-100 d-flex align-items-center bg-2">
         <Statistics stickers={stickers} data={statisticsection} />
       </Container>
-      <Container wrapperClass="py-5 d-flex align-items-center bg-3">
+      <Container wrapperClass="header-wrapper py-5 d-flex align-items-center bg-3">
         <GetStickerCTA data={ctasection} />
       </Container>
       {/* <Container wrapperClass="d-flex align-items-center bg-2"><LatestNews posts={posts} /></Container> */}
