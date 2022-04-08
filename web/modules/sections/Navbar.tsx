812bffe95bba7ed4a8e2aad43473b945c7dba6be
Jesse Alvarado <contact@jessealvarado.com>
2022-04-08 09:10:09 +0900

    fix logo centering horizontal issue

--- web/modules/sections/Navbar.tsx
+++ web/modules/sections/Navbar.tsx
@@ -5,45 +5,83 @@ import cookieCutter from 'cookie-cutter'
 import Image from 'next/image'
 
 export default function Navbar() {
-    const router = useRouter()
+  const router = useRouter()
 
-    return (
+  return (
     <nav className="navbar navbar-expand-md navbar-light border-bottom">
-        <div className="container">
+      <div className="container">
         <Link href="/">
-            <a className="navbar-brand">
-               <Image src={'/ecotowari-logo.png'}  width="125" height="40" objectFit={'scale-down'} alt="Background" quality={10} priority={true} />
-            </a>
-            </Link>
-            <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNavbar" aria-controls="collapseNavbar" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="collapseNavbar">
-                <ul className="navbar-nav ms-auto">
-            
-                </ul>
-                <ul className="navbar-nav ms-auto">
-                    {/* <li className="nav-item active">
+          <a className="navbar-brand d-flex justify-content-center">
+            <Image
+              src={'/ecotowari-logo.png'}
+              width="120"
+              height="40"
+              objectFit={'contain'}
+              alt="Ecotowari logo"
+              quality={10}
+              priority={true}
+            />
+          </a>
+        </Link>
+        <button
+          className="navbar-toggler ms-auto"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseNavbar"
+          aria-controls="collapseNavbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="collapseNavbar">
+          <ul className="navbar-nav ms-auto"></ul>
+          <ul className="navbar-nav ms-auto">
+            {/* <li className="nav-item active">
                         <Link href="/mission"><a className="nav-link">{router.locale === 'ja' ? '我々の活動' : 'Impact Of Flyers'}</a></Link>
                     </li> */}
-                    <li className="nav-item">
-                        <Link href="/#sticker"><a className="nav-link">{router.locale === 'ja' ? 'ステッカーの入手' : 'Get Sticker'}</a></Link>
-                    </li>
-                    {/* <li className="nav-item">
+            <li className="nav-item">
+              <Link href="/#sticker">
+                <a className="nav-link">
+                  {router.locale === 'ja' ? 'ステッカーの入手' : 'Get Sticker'}
+                </a>
+              </Link>
+            </li>
+            {/* <li className="nav-item">
                         <Link href="/news"><a className="nav-link">{router.locale === 'ja' ? '最新情報' : 'Latest News'}</a></Link>
                     </li> */}
-                    <li className="nav-item">
-                        <Link href="/contact"><a className="nav-link">{router.locale === 'ja' ? '問い合わせ' : 'Contact'}</a></Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link href={router.asPath} locale='ja'><a className="nav-link" onClick={()=>{cookieCutter.set('NEXT_LOCALE', 'ja', new Date(999999999999))}}>JP</a></Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link href={router.asPath} locale='en'><a className="nav-link" onClick={()=>{cookieCutter.set('NEXT_LOCALE', 'en')}}>EN</a></Link>
-                    </li>
-                </ul>
-            </div>
+            <li className="nav-item">
+              <Link href="/contact">
+                <a className="nav-link">{router.locale === 'ja' ? '問い合わせ' : 'Contact'}</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href={router.asPath} locale="ja">
+                <a
+                  className="nav-link"
+                  onClick={() => {
+                    cookieCutter.set('NEXT_LOCALE', 'ja', new Date(999999999999))
+                  }}
+                >
+                  JP
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href={router.asPath} locale="en">
+                <a
+                  className="nav-link"
+                  onClick={() => {
+                    cookieCutter.set('NEXT_LOCALE', 'en')
+                  }}
+                >
+                  EN
+                </a>
+              </Link>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
-    )
+  )
 }
