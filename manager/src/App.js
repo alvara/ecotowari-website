c670b5f931b9d03f0f6a7fb703437ffe41b8626a
Jesse Alvarado <contact@jessealvarado.com>
2022-05-14 23:12:45 +0900

    connect to supabase and display data

---
+++ manager/src/App.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import { supabase } from "./client";
+
+function App() {
+  // array of stickers from database
+  const [stickers, setStickers] = React.useState([]);
+
+  // track current sticker being created/edited
+  const [sticker, setSticker] = React.useState({
+    qty: 1,
+    notes: "",
+    started_at: new Date(),
+  });
+
+  // destructuring sticker object
+  const { qty, notes, started_at } = sticker;
+
+  // get stickers from database
+  async function fetchStickers() {
+    const { data } = await supabase.from("stickers").select();
+    setStickers(data);
+  }
+
+  // call fetch when page loads
+  useEffect(() => {
+    fetchStickers();
+  }, []);
+
+  console.log(stickers);
+
+  return <div className="App">{JSON.stringify(stickers)}</div>;
+}
+
+export default App;
