14378c9eee74125a0f16e547dcc3bbf19e04a894
Jesse Alvarado <contact@jessealvarado.com>
2022-05-23 00:37:39 +0900

    change font to noto sans jp

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 // pages/_document.js
 import {Html, Head, Main, NextScript} from 'next/document'
@@ -10,7 +10,10 @@ export default function Document() {
         {/* Google Font */}
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <body>
         <Main />
