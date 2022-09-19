c52917c107a84ed14e7cb835656bd89377d04224
Jesse Alvarado <contact@jessealvarado.com>
2022-09-20 01:49:57 +0900

    fix: remove groq and supabase

--- utils/supabase.tsx
+++
@@ -1,7 +0,0 @@
-// used across application for accessing the data here.
-import { createClient } from '@supabase/supabase-js';
-
-export default createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_API_KEY
-);
