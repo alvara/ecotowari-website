76eae2bfb2ba068f9ae67b03666404c5202672d0
Jesse Alvarado <contact@jessealvarado.com>
2022-09-20 01:09:06 +0900

    feat: remove unused packages and update react

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
-import groq from 'groq';
 import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
-
-import client from '../client';
 import Container from '../components/Container';
 import HeroHeader from '../components/header/HeroHeader';
 import ContactUs from '../features/sections/ContactUs';
 
-export async function getStaticProps() {
-  const contactPage = await client.fetch(groq`
-      *[_type == "contact-page"] | order(publishedAt desc)
-    `);
-
-  return {
-    props: {
-      contactPage,
-    },
-  };
-}
-
 const Contact = ({ contactPage }) => {
   const router = useRouter();
-  const { headersection, formsection } = contactPage[0];
 
   return (
     <>
